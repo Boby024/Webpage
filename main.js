6c3bc531e98968bbdbaf1f6f1af89b39a489ff18
Boby024 <william.kana-tsoplefack@stud.uni-due.de>
2020-02-29 14:49:17 +0100

    caeser converter File with Js drag and drog file

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
 
-let projekt = [
-    {"titel":"Erstellung einer Buecherverwaltung",
-    "beschreibung":" <div class=elementToggle> "+
-        "                        Im Rahmen dieser Arbeit solle ich eine Webanwendung erstellen, die eine Bücherverwaltung realisiert." +
-        "                Benutzer sollen sich an einem Online-System anmelden und über eine Weboberfläche einen Bücherkatalog einsehen,"+
-        "                verwalten sowie Bücher ausleihen können. Darüber hinaus soll das System eine geeignete REST-Schnittstelle zur Verfügung stellen," +
+let projects = [
+    {
+        "titel":"+ Caesar-Converter-File: JavaScript and Flask",
+        "beschreibung":"Zum Verschlüsseln (mit Key) eines Texts oder File (.txt extension) mit Caeser-Encryption"+
+            "<p>using Drag and Drog <br>oder the standart input methode from Html5</p>"+
+            "<p>Note: Momentan funktiniert nur mit txt File (Implementieren für PDF kommt später). Diese Voraussetzungen wurden gecheckt:\n</p>"+
+            "<p>File unter 1 MB => gesandt mit REST-API <br>JavaScript checkt, if File (.txt) ist ; und auch Html5</p>"+
+            "<p>Hier das <a href=\"https://github.com/Boby024/Caesar-Converter-File\" target=\"_blank\">Github-Repository</a></p> "
+    },
+    {"titel":"+ Erstellung einer Buecherverwaltung: Flask + Angular",
+    "beschreibung":" Im Rahmen dieser Übungsaufgabe solle ich eine Webanwendung erstellen, die eine Bücherverwaltung realisiert.\n" +
+        "                Benutzer sollen sich an einem Online-System anmelden und über eine Weboberfläche einen Bücherkatalog einsehen,\n" +
+        "                verwalten sowie Bücher ausleihen können. Darüber hinaus soll das System eine geeignete REST-Schnittstelle zur Verfügung stellen,\n" +
         "                die ebenfalls einige dieser Funktionen anbietet. Eine Umsetzung dieser Anwendung erfordert sowohl eine Backend- als auch eine FrontendImplementierung.\n" +
-        "                Darüber hinaus ist eine Datenbankanbindung erforderlich, damit die Daten persistent gespeichert werden können." +
-        "                <p>So sieht des Projekts <a href="+"files/Buecherverwaltung.pdf"+" target=_blank>Bild</a> aus </p>" +
-        "                <p>Hier das <a href="+"https://github.com/Boby024/Book-Management/tree/fullversion"+" title="+"Bücherverwaltung"+">Github-Repository (branch: fullversion)</a></p> </br>" +
-        "                </p>" +
-        "                    </div>"
+        "                Darüber hinaus ist eine Datenbankanbindung erforderlich, damit die Daten persistent gespeichert werden können.\n" +
+        "                 <p>So sieht des Projekts <a href=\"files/Buecherverwaltung.pdf\" target=\"_blank\">Bild</a> aus </p>\n" +
+        "                <p>Hier das <a href=\"https://github.com/Boby024/Book-Management/tree/fullversion\" target=\"_blank\" title=\"Bücherverwaltung\">Github-Repository (branch: fullversion)</a></p> </br>\n"
     },
     {
-        "titel":"Voice Queries in Product Search with Vague Conditions",
-        "beschreibung":"hi"
+        "titel":"+ Voice Queries in Product Search with Vague Conditions: Flask + Angular",
+        "beschreibung":"Mein Praxisprojekt, an den ich gerade arbeite ist verfügbar auf diesem\n" +
+            "                    <a href=\"https://github.com/KalleNolte/Laptopshop\" target=\"_blank\" title=\"project\"><strong> Github-Repository </strong></a>\n" +
+            "                    Es geht um  eine Webseite, die die Suche schneller mit weniger Informationen vom User ermöglicht.\n" +
+            "                    Das Projekt wird realisiert mit Angular für Front-end und Python für Backend.\n" +
+            "                    <p>Auf diesem Projekt arbeite ich mehr im Front-End als im Backend</p>\n" +
+            "                    Mehr über das Projekt  \" Voice Queries in Product Search with Vague Conditions \", hier :\n" +
+            "                    <p ><a href=\"http://www.is.informatik.uni-duisburg.de/courses/pg_2_ss19/index.html\" title=\"about\" target=\"_blank\">http://www.is.informatik.uni-duisburg.de/courses/pg_2_ss19/index.html </a></p>\n" +
+            "                    <p>Hier ist das Mockup des Projekts <a href=\"images/lap-1.jpg\" target=\"_blank\">Bild</a> oder  <a href=\"files/Laptopshop.pdf\" target=\"_blank\">pdf</a> </p>\n" +
+            "                    <div>\n" +
+            "                        <strong>Front-end :</strong> Angular\n" +
+            "                        <p> Html5, css3, javaScript, management of data from backend</p>\n" +
+            "                    </div>\n" +
+            "                    <div>\n" +
+            "                        <strong>Backend :</strong> Flask\n" +
+            "                        <p> Elasticsearch und Kibana für die Visualisation  von Daten </br>\n" +
+            "                        Flask für die Verbindung zwischen Front-end und Backend   </p>\n" +
+            "                    </div>"
+    },
+    {
+        "titel":"+ Musik-Player mit javaFx",
+        "beschreibung":"Im Rahmen der Übung soll im Semester ein Musikplayer erstellt werden, der in der Lage, ist Lieder von der\n" +
+            "                Festplatte ins Programm einzulesen, Lieder einer Playlist hinzuzufügen bzw. zu löschen und diese Playlist\n" +
+            "                abzuspielen. Auÿerdem sollen Informationen Titel zu dem ausgewählten Lied angezeigt\n" +
+            "                werden, und es soll möglich sein diese Informationen zu ändern.\n" +
+            "                <p><strong>Kurzer Hinweis:</strong> Auf diesem Projekt wurde mehr Wert auf Funktionaliäten der Anwendung als das Aussehen :) gelegt</p>\n" +
+            "                <p>So sieht des Projekts <a href=\"images/player_2.jpg\" target=\"_blank\">Bild</a> aus </p>\n" +
+            "                <p>Hier das <a href=\"https://github.com/Boby024/Musik_player_with_javafx\" target=\"_blank\">Github-Repository</a></p>"
     }
-]
+];
 
 function toggleEl(selbst) {
 
@@ -66,11 +97,22 @@ function sendMail() {
 
     window.location.href = "index.html";
 }
+function showAllprojects(){
+    let finalForm = "";
+    for(let i = 0; i < projects.length; i++){
+        let stand = 'beschreibung-'+i;
+        let titel= "<div class='elementT'><strong>"+projects[i].titel+"</strong></div>";
+        let beschreibung = "<div id="+stand+">"+projects[i].beschreibung+"</div>";
+        finalForm += titel + beschreibung;
+    }
+    document.getElementById('toggleProject').innerHTML = finalForm;
+}
 
 // document.querySelector('#sendMe').addEventListener('click', sendMail);
 document.addEventListener("DOMContentLoaded",()=>{
+    this.showAllprojects();
     (function($) { $(document).ready(function(){
-            $('#beschreibung-1, #beschreibung-2, #beschreibung-3').hide();
+            $('#beschreibung-0, #beschreibung-1, #beschreibung-2, #beschreibung-3').hide();
             $('.elementT').click(function(){
                 $(this).next().slideToggle();
                 $(this).toggleClass("afterToggle");
